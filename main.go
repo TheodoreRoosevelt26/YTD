1baa2aa1354648ee3a1306ef8cc6b64f3ad05784
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-12-05 11:22:35 +0200

    Put program in for loop, replaced system panics, added some ideas for future changes

--- main.go
+++ main.go
@@ -13,61 +13,71 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	// removeVideo := true
+	// location?
+	for {
+		reader := bufio.NewReader(os.Stdin)
 
-	fmt.Printf("████████████████████████████████████████████████████████████████████████████████████████████████████████████\n█▄─█─▄█─▄▄─█▄─██─▄█─▄─▄─█▄─██─▄█▄─▄─▀█▄─▄▄─█▄─▄▄▀█─▄▄─█▄─█▀▀▀█─▄█▄─▀█▄─▄█▄─▄███─▄▄─██▀▄─██▄─▄▄▀█▄─▄▄─█▄─▄▄▀█\n██▄─▄██─██─██─██─████─████─██─███─▄─▀██─▄█▀██─██─█─██─██─█─█─█─███─█▄▀─███─██▀█─██─██─▀─███─██─██─▄█▀██─▄─▄█\n▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀\n")
+		fmt.Printf("\n████████████████████████████████████████████████████████████████████████████████████████████████████████████\n█▄─█─▄█─▄▄─█▄─██─▄█─▄─▄─█▄─██─▄█▄─▄─▀█▄─▄▄─█▄─▄▄▀█─▄▄─█▄─█▀▀▀█─▄█▄─▀█▄─▄█▄─▄███─▄▄─██▀▄─██▄─▄▄▀█▄─▄▄─█▄─▄▄▀█\n██▄─▄██─██─██─██─████─████─██─███─▄─▀██─▄█▀██─██─█─██─██─█─█─█─███─█▄▀─███─██▀█─██─██─▀─███─██─██─▄█▀██─▄─▄█\n▀▀▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▀▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀\n")
 
-	client := youtube.Client{}
-	fmt.Println("Please paste and enter the video ID")
-	videoID, _ := reader.ReadString('\n')
-	videoID = strings.TrimSpace(videoID)
-	fmt.Println("Put in the title for your file")
-	videoTitle, _ := reader.ReadString('\n')
-	videoTitle = strings.TrimSpace(videoTitle)
-	if videoID == "" {
-		videoID = "dQw4w9WgXcQ"
+		client := youtube.Client{}
+		fmt.Println("Please paste and enter the video ID")
+		videoID, _ := reader.ReadString('\n')
+		videoID = strings.TrimSpace(videoID)
+		fmt.Println("Put in the title for your file")
+		videoTitle, _ := reader.ReadString('\n')
+		videoTitle = strings.TrimSpace(videoTitle)
+		if videoID == "" {
+			videoID = "dQw4w9WgXcQ"
+		}
+		if videoTitle == "" {
+			videoTitle = "Rick Astley - Never Gonna Give You Up"
+		}
+
+		fmt.Println("Extracting video...")
+
+		video, err := client.GetVideo(videoID)
+		if err != nil {
+			fmt.Println("Wrong URL / video ID.\nPaste either full link, e.g.: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', or the video ID, e.g.: 'dQw4w9WgXcQ'")
+			time.Sleep(3 * time.Second)
+			continue
+		}
+
+		formats := video.Formats.WithAudioChannels()
+		stream, _, err := client.GetStream(video, &formats[0])
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+		defer stream.Close()
+
+		file, err := os.Create("video.mp4")
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+		defer file.Close()
+
+		_, err = io.Copy(file, stream)
+		if err != nil {
+			fmt.Print(err)
+			continue
+		}
+		fmt.Println("Video extracted successfully")
+
+		fmt.Println("Extracting audio...")
+		cmd := exec.Command("ffmpeg", "-i", "video.mp4", "-vn", "-ab", "128k", "-ar", "44100", "-y", videoTitle+".mp3")
+		err = cmd.Run()
+		if err != nil {
+			fmt.Println("Error extracting audio:", err)
+			return
+		}
+
+		fmt.Println("Audio extracted successfully")
+
+		// add: if statement to check removeVideo
+
+		time.Sleep(1 * time.Second)
+
 	}
-	if videoTitle == "" {
-		videoTitle = "Rick Astley - Never Gonna Give You Up"
-	}
-
-	fmt.Println("Extracting video...")
-
-	video, err := client.GetVideo(videoID)
-	if err != nil {
-		fmt.Println("Wrong URL / video ID.\nPaste either full link, e.g.: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', or the video ID, e.g.: 'dQw4w9WgXcQ'")
-		time.Sleep(3 * time.Second)
-		main()
-	}
-
-	formats := video.Formats.WithAudioChannels()
-	stream, _, err := client.GetStream(video, &formats[0])
-	if err != nil {
-		panic(err)
-	}
-	defer stream.Close()
-
-	file, err := os.Create("video.mp4")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, stream)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Video extracted successfully")
-
-	fmt.Println("Extracting audio...")
-	cmd := exec.Command("ffmpeg", "-i", "video.mp4", "-vn", "-ab", "128k", "-ar", "44100", "-y", videoTitle+".mp3")
-	err = cmd.Run()
-	if err != nil {
-		fmt.Println("Error extracting audio:", err)
-		return
-	}
-
-	fmt.Println("Audio extracted successfully")
-	time.Sleep(1 * time.Second)
-	main()
 }
