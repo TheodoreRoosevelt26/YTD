397a0960d546e1fac972b126f8f3d32b38312b0e
TheodoreRoosevelt26 <TR.theodore.roosevelt@proton.me>
2024-12-05 17:23:34 +0200

    Made Error statements more descriptive, added a bytesCopied check, investigated current package error

--- main.go
+++ main.go
@@ -22,23 +22,23 @@ func main() {
 
 		client := youtube.Client{}
 		fmt.Println("Please paste and enter the video ID")
-		videoID, _ := reader.ReadString('\n')
-		videoID = strings.TrimSpace(videoID)
+		videoIDInput, _ := reader.ReadString('\n')
+		videoID := strings.TrimSpace(videoIDInput)
 		fmt.Println("Put in the title for your file")
 		videoTitle, _ := reader.ReadString('\n')
-		videoTitle = strings.TrimSpace(videoTitle)
+		fileTitle := strings.TrimSpace(videoTitle)
 		if videoID == "" {
 			videoID = "dQw4w9WgXcQ"
 		}
-		if videoTitle == "" {
-			videoTitle = "Rick Astley - Never Gonna Give You Up"
+		if fileTitle == "" {
+			fileTitle = "Rick Astley - Never Gonna Give You Up"
 		}
 
 		fmt.Println("Extracting video...")
 
 		video, err := client.GetVideo(videoID)
 		if err != nil {
-			fmt.Println("Wrong URL / video ID.\nPaste either full link, e.g.: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', or the video ID, e.g.: 'dQw4w9WgXcQ'")
+			fmt.Println("ERROR GETTING VIDEO: Wrong URL / video ID.\nPaste either full link, e.g.: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', or the video ID, e.g.: 'dQw4w9WgXcQ'")
 			time.Sleep(3 * time.Second)
 			continue
 		}
@@ -46,31 +46,36 @@ func main() {
 		formats := video.Formats.WithAudioChannels()
 		stream, _, err := client.GetStream(video, &formats[0])
 		if err != nil {
-			fmt.Print(err)
+			fmt.Printf("ERROR GETTING VIDEO STREAM: %s", err)
 			continue
 		}
 		defer stream.Close()
 
 		file, err := os.Create("video.mp4")
 		if err != nil {
-			fmt.Print(err)
+			fmt.Printf("ERROR CREATING VIDEO FILE: %s", err)
 			continue
 		}
 		defer file.Close()
 
-		_, err = io.Copy(file, stream)
+		bytesCopied, err := io.Copy(file, stream)
 		if err != nil {
-			fmt.Print(err)
+			fmt.Printf("ERROR CREATING VIDEO: %s", err)
 			continue
 		}
+		if bytesCopied == 0 {
+			fmt.Println("ERROR UNABLE TO RECEIVE VIDEO STEAM: Stream copying resulted in 0 bytes.")
+			continue
+		}
+
 		fmt.Println("Video extracted successfully")
 
 		fmt.Println("Extracting audio...")
-		cmd := exec.Command("ffmpeg", "-i", "video.mp4", "-vn", "-ab", "128k", "-ar", "44100", "-y", videoTitle+".mp3")
+		cmd := exec.Command("ffmpeg", "-i", "video.mp4", "-vn", "-ab", "128k", "-ar", "44100", "-y", fileTitle+".mp3")
 		err = cmd.Run()
 		if err != nil {
 			fmt.Println("Error extracting audio:", err)
-			return
+			continue
 		}
 
 		fmt.Println("Audio extracted successfully")
@@ -78,6 +83,5 @@ func main() {
 		// add: if statement to check removeVideo
 
 		time.Sleep(1 * time.Second)
-
 	}
 }
